92e43d93c35f4974e19f6385ef9367a531724f88
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-01-20 19:15:49 -0500

    (SIMP-2469) Fixed tcpwrappers boolean
    This had not been captured from simp_options::tcpwrappers yet
    
    SIMP-2469 #comment Updated to work with simp_options::tcpwrappers

--- spec/classes/server/global_spec.rb
+++ spec/classes/server/global_spec.rb
@@ -12,7 +12,17 @@ describe 'rsync::server::global' do
       context "on #{os}" do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to create_concat__fragment('rsync_global').with_content(/address = 127.0.0.1/) }
-        it { is_expected.to create_tcpwrappers__allow('rsync') }
+        it { is_expected.to_not create_tcpwrappers__allow('rsync') }
+
+        context 'with tcpwrappers' do
+          let(:params) {{
+            :tcpwrappers => true
+          }}
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to create_concat__fragment('rsync_global').with_content(/address = 127.0.0.1/) }
+          it { is_expected.to create_tcpwrappers__allow('rsync') }
+        end
       end
     end
   end
