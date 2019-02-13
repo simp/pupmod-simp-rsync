b262de7a987691ae2a9b307f4f3344e39701ebd8
lnemsick-simp <lnemsick.simp@gmail.com>
2019-02-13 09:59:55 -0500

    (SIMP-6110) Use (namespaced) simplib::passgen
    - Fixed a template bug that prevented catalogue compilation when
      rsync::server::section::user_pass was set
    - Use simplib::passgen() in lieu of passgen(), a deprecated simplib
      Puppet 3 function.
    
    SIMP-6110 #commpent pupmod-simp-rsync

--- spec/defines/server/section_spec.rb
+++ spec/defines/server/section_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe 'rsync::server::section' do
-  context 'supported operating systems' do
-    on_supported_os.each do |os, os_facts|
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
       let(:title) { 'test' }
 
       let(:facts) { os_facts }
@@ -11,13 +11,47 @@ describe 'rsync::server::section' do
         'include "::rsync::server"'
       }
 
-      let(:params) {{
-        :path => '/test/dir'
-      }}
+      context 'with default parameters' do
+        let(:params) {{
+          :path => '/test/dir'
+        }}
 
-      context "on #{os}" do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to create_concat__fragment("rsync_#{title}.section") }
+        it { is_expected.to_not create_file("/etc/rsync/#{title}.rsyncd.secrets") }
+      end
+
+      context 'with user_pass and comment parameters set' do
+        let(:params) {{
+          :path       => '/test/dir',
+          :user_pass  => [ 'user1:user1password', 'user2:user2password', 'skipme'],
+          :comment    => 'section TEST'
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to create_file("/etc/rsync/#{title}.rsyncd.secrets").with(
+            :ensure    => 'file',
+            :owner     => 'root',
+            :group     => 'root',
+            :mode      => '0600',
+            :show_diff => false,
+            :content   => <<-EOM
+user1:user1password
+user2:user2password
+            EOM
+          )
+        end
+      end
+
+      context 'with auth_users parameter set' do
+        let(:params) {{
+          :path       => '/test/dir',
+          :auth_users => [ 'authuser1', 'authuser2'],
+        }}
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_file("/etc/rsync/#{title}.rsyncd.secrets").with_content(/^authuser1:/) }
+        it { is_expected.to create_file("/etc/rsync/#{title}.rsyncd.secrets").with_content(/^authuser2:/) }
       end
     end
   end
