ce698c119a191c7779b84c16efac7ba6e051be1e
Chris Tessmer <chris.tessmer@onyxpoint.com>
2023-07-10 19:04:30 -0400

    Add more Rocky

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe 'rsync' do
+  def mock_selinux_enforcing_facts(os_facts)
+    os_facts[:selinux] = true
+    os_facts[:os][:selinux][:config_mode] = 'enforcing'
+    os_facts[:os][:selinux][:config_policy] = 'targeted'
+    os_facts[:os][:selinux][:current_mode] = 'enforcing'
+    os_facts[:os][:selinux][:enabled] = true
+    os_facts[:os][:selinux][:enforced] = true
+    os_facts
+  end
+
   def mock_selinux_disabled_facts(os_facts)
     os_facts[:selinux] = false
     os_facts[:os][:selinux][:config_mode] = 'disabled'
@@ -18,6 +28,11 @@ describe 'rsync' do
         it { is_expected.to create_class('rsync') }
 
         context 'base' do
+          let(:facts) do
+            os_facts = os_facts.dup
+            os_facts = mock_selinux_enforcing_facts(os_facts)
+            os_facts
+          end
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_class('rsync::selinux') }
           it { is_expected.to create_selboolean('rsync_client') }
