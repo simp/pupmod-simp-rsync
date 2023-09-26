0b514d6ae31f4ae4a9eb721c449f4886d21d9e39
Steven Pritchard <steve@sicura.us>
2023-09-26 23:29:24 +0200

    Add AlmaLinux 8 support (#102)
    Additionally:
    - Update module dependencies
    - Add Puppet 8 support
    - Drop Puppet 6 support

--- manifests/selinux.pp
+++ manifests/selinux.pp
@@ -3,28 +3,25 @@
 # @author https://github.com/simp/pupmod-simp-selinux/graphs/contributors
 #
 class rsync::selinux {
+  $_sebool_anon_write    = $rsync::sebool_anon_write ? { true => 'on', default => 'off' }
+  $_sebool_client        = $rsync::sebool_client ? { true => 'on', default => 'off' }
+  $_sebool_export_all_ro = $rsync::sebool_export_all_ro ? { true => 'on', default => 'off' }
+  $_sebool_full_access   = $rsync::sebool_full_access ? { true => 'on', default => 'off' }
 
-  $_sebool_anon_write    = $::rsync::sebool_anon_write ? { true => 'on', default => 'off' }
-  $_sebool_client        = $::rsync::sebool_client ? { true => 'on', default => 'off' }
-  $_sebool_export_all_ro = $::rsync::sebool_export_all_ro ? { true => 'on', default => 'off' }
-  $_sebool_full_access   = $::rsync::sebool_full_access ? { true => 'on', default => 'off' }
-
-  if $facts[os][name] in ['RedHat','CentOS','OracleLinux','Amazon','Rocky'] {
-    selboolean { 'rsync_client':
-      persistent => true,
-      value      => $_sebool_client
-    }
-    selboolean { 'rsync_export_all_ro':
-      persistent => true,
-      value      => $_sebool_export_all_ro
-    }
-    selboolean { 'rsync_anon_write':
-      persistent => true,
-      value      => $_sebool_anon_write
-    }
-    selboolean { 'rsync_full_access':
-      persistent => true,
-      value      => $_sebool_full_access
-    }
+  selboolean { 'rsync_client':
+    persistent => true,
+    value      => $_sebool_client
+  }
+  selboolean { 'rsync_export_all_ro':
+    persistent => true,
+    value      => $_sebool_export_all_ro
+  }
+  selboolean { 'rsync_anon_write':
+    persistent => true,
+    value      => $_sebool_anon_write
+  }
+  selboolean { 'rsync_full_access':
+    persistent => true,
+    value      => $_sebool_full_access
   }
 }
