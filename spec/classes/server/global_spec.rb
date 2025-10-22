98f8d02dc84edc627c50ee96bd84a0cd1d050107
Steven Pritchard <steve@sicura.us>
2025-10-22 15:38:18 -0500

    Fix incorrect `let` use in rspec tests

--- spec/classes/server/global_spec.rb
+++ spec/classes/server/global_spec.rb
@@ -8,9 +8,9 @@ describe 'rsync::server::global' do
 
   context 'supported operating systems' do
     on_supported_os.each do |os, os_facts|
-      let(:facts) { os_facts }
-
       context "on #{os}" do
+        let(:facts) { os_facts }
+
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to create_concat__fragment('rsync_global').with_content(%r{address = 127.0.0.1}) }
         it { is_expected.not_to create_tcpwrappers__allow('rsync') }
--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe 'rsync::server' do
   context 'supported operating systems' do
     on_supported_os.each do |os, os_facts|
-      let(:facts) { os_facts }
-
       context "on #{os}" do
+        let(:facts) { os_facts }
+
         it { is_expected.to compile.with_all_deps }
 
         it { is_expected.to create_class('rsync') }
