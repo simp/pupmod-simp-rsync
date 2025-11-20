fe002a373f3d8bf02e0d40bce269cc2a372c4c0e
Steven Pritchard <steve@sicura.us>
2025-11-20 16:34:08 -0600

    Fix ordering

--- spec/defines/retrieve_spec.rb
+++ spec/defines/retrieve_spec.rb
@@ -16,8 +16,8 @@ describe 'rsync::retrieve' do
       }
     end
 
-    context "on #{os}" do
-      on_supported_os.each do |os, os_facts|
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
         let(:facts) { os_facts }
 
         it { is_expected.to compile.with_all_deps }
